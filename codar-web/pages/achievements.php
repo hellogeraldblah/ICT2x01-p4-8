79e3ba5cd15f4d91a444bf30863b17b2c679e022
hellogeraldblah <gxraldpeh@gmail.com>
2021-11-06 16:08:42 +0800

    First upload of web template

---
+++ codar-web/pages/achievements.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php $page="Achievements"; ?>
+
+<body class="g-sidenav-show bg-gray-100">
+
+  <!-- Side Panel -->
+  <?php include "sidepanel.php" ?>
+  <!-- End Side Panel -->
+
+  <div class="main-content position-relative bg-gray-100 max-height-vh-100 h-100">
+
+    <!-- Navbar -->
+    <?php include "navbar.php" ?>
+    <!-- End Navbar -->
+  </div>
+
+  </body
